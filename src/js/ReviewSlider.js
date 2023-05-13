ebc46add5276300a3da122ab4be94e596e5f84dc
Sankovich Ilya <ilyhasankovich@gmail.com>
2023-05-13 14:37:35 +0300

    travellers slider

---
+++ src/js/ReviewSlider.js
@@ -0,0 +1,89 @@
+class ReviewSlider {
+    windowResize() {
+        if(window.innerWidth > 820) {
+            this.autoSwiping(this.slidesAmount, 2, 2);
+        }
+        if(window.innerWidth < 820 && window.innerWidth > 480) {
+            this.slidesAmount = 2;
+            this.createPagination(this.slideIndex, this.slidesAmount);
+            this.autoSwiping(this.slidesAmount, 4, 4);
+        }
+        if(window.innerWidth < 480 && window.innerWidth > 0) {
+            this.slidesAmount = 1;
+            this.createPagination(this.slideIndex, this.slidesAmount);
+            this.autoSwiping(this.slidesAmount, 8, 8);
+        }
+    }    
+
+    getItemSize() {
+        const listItem = document.querySelector('.carousel-list__item');
+        const listItemWidth = listItem.offsetWidth;
+
+        return(listItemWidth);
+    }
+
+    scroll(slidesAmount, factor) {
+        this.carousel.scrollTo({ left: this.carousel.scrollLeft + slidesAmount * factor * this.getItemSize(),
+            behavior: "smooth",
+        });
+    }
+
+    autoSwiping(slidesAmount, factor, limit) {
+        setInterval(() => {
+            if(this.slideIndex < limit) {
+                this.slideIndex += 1;
+                this.updatePagination(this.slideIndex);
+                this.scroll(slidesAmount, 1);
+            } else {
+                this.slideIndex = 0;
+                this.updatePagination(this.slideIndex);
+                this.scroll(slidesAmount, -factor);
+            }
+        }, 4000); 
+    }
+
+    createPagination(slideIndex, slidesAmount) {  
+        this.pagination.innerHTML = '';      
+        for (let i = 0; i < Math.round(this.carousel.children.length / slidesAmount); i++) {
+            console.log(Math.floor(this.carousel.children.length / slidesAmount));
+            const button = document.createElement('button');
+            button.addEventListener('click', () => this.slideTo(i));
+            this.pagination.appendChild(button);
+        }
+        this.updatePagination(slideIndex);
+    }
+
+    updatePagination(slideIndex) {
+        const buttons = this.pagination.querySelectorAll('button');
+        buttons.forEach((button, index) => {
+            if (index === slideIndex) {
+              button.classList.add('active');
+            } else {
+              button.classList.remove('active');
+            }
+        });
+    }
+
+    slideTo(index) {
+        if(index > this.slideIndex) {
+            this.scroll((index - this.slideIndex), this.slidesAmount);
+            this.updatePagination(index);
+            this.slideIndex = index;
+        } else {
+            this.scroll(-(this.slideIndex - index), this.slidesAmount);
+            this.updatePagination(index);
+            this.slideIndex = index;
+        }
+    }
+
+    constructor() {
+        this.slidesAmount = 3;
+        this.slideIndex = 0;
+        this.pagination = document.querySelector('.review-pagination');
+        this.carousel = document.querySelector('.carousel__list');
+        this.createPagination(this.slideIndex, this.slidesAmount);
+        this.windowResize();
+    }
+}
+
+export default ReviewSlider;
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import HeroSlider from './HeroSlider'
+import ReviewSlider from './ReviewSlider';
 
 document.addEventListener('DOMContentLoaded', () => {
     const heroSlider = new HeroSlider();
-
+    const reviewSlider = new ReviewSlider();
+    
 })
